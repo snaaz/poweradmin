89027c7ee5ef410a176de64ab8bc78cc0dfbc5dd
snaaz <snaaz021@gmail.com>
2016-12-05 21:28:39 +0530

    reset password

--- forgot_password.php
+++ forgot_password.php
@@ -2,51 +2,52 @@
 include('db.php');
 
    if(isset($_POST['reset_password']))
-   {
+ {
 	$new_password=$_POST['new_password'];
 	$confirm_password=$_POST['confirm_password'];
-	$id=$_POST['id'];
-	
-	$cost = 10;
+	//echo "<br>";
+	//echo $confirm_password;
+	$id=$_POST['encrypt'];
+	//echo $url;
+
+$cost = 10;
     $salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
     $salt = sprintf("$2a$%02d$", $cost) . $salt;
     $hash = crypt($confirm_password, $salt);
 	//2nd last
 	if($new_password==$confirm_password)
 	{
-	$reset=mysqli_query($connection,"UPDATE users SET  password ='".$hash."' WHERE id = '".$id."'");
-		 
-	//echo "c";
-	//echo $Fname_save;
-	//echo $Last_save;
-	//echo $_SESSION['user']['password'];
-	//echo $confirm_password;
-	//echo $id;
-	//last
+   // $reset1=mysqli_query($connection,"update users set encrypt_id=null");
+	$reset=mysqli_query($connection,"UPDATE users SET  password ='".$hash."' WHERE encrypt_id = '".$id."'");
+	$reset2=mysqli_query($connection,"update users set encrypt_id=null");
 	if($reset)
   {
   $msg="Successfully Updated!!";
   echo $msg;
-  $_SESSION["message"]="PASSWORD CHANGE SUECCSESSFULLY PLEASE LOGIN";
+  session_start();
+  $_SESSION["message"]="PASSWORD CHANGE SUECCSESSFULLY";
   header("Location:login.html");
   
   }
-  //last
+  
     else 
    {
 	echo "Error: " . $updated. "<br>" . $connection->error;
 	$_SESSION['error'] = "Error: " . $updated . "<br>" . $connection->error;
-	header('Location:forgot_password.html');
+	header('Location:http://localhost.myapps/poweradmin/forgot_password.html?encrypt='.$id.'&action=reset');
 	}
 	 
 	}
 	//2nd last
  else
 	{
-		 header('Location:forgot_password.html');
+		 header('Location:http://localhost.myapps/poweradmin/forgot_password.html?encrypt='.$id.'&action=reset');
 		 echo "confirm password must be same";
 	 }
  }
-	  
+	
+	
+ 
+	 
 
 ?>
--- sendmail.php
+++ sendmail.php
@@ -13,29 +13,31 @@ if(isset($_POST['reset']))
         $query = "SELECT id FROM users where email='".$email."'";
         $result = mysqli_query($connection,$query);
         $Results = mysqli_fetch_array($result);
-	   // $_SESSION[""]
+	   
  
         if(count($Results)>=1)
         {
             $encrypt = md5(1290*3+$Results['id']);
+			$sql=mysqli_query($connection,"update users set encrypt_id='".$encrypt."' where email='".$email."'" );
             $message = "Your password reset link send to your e-mail address.";
             $to=$email;
             $subject="Forget Password";
-            $from = 'info@phpgang.com';
-            $body='Hi, <br/> <br/>Your Membership ID is '.$Results['id'].' <br><br>Click here to reset your password http://localhost.myapps/poweradmin/forgot_password.html?encrypt='.$encrypt.'&action=reset   <br/> <br/>--<br>PHPGang.com<br>Solve your problems.';
+            $from = 'Shagufta Naaz';
+            $body='Hi, <br/> <br/>Your Membership ID is '.$Results['id'].' <br><br>Click here to reset your password http://localhost.myapps/poweradmin/forgot_password.html?encrypt='.$encrypt.'&action=reset   <br/> <br/>--<br>';
             $headers = "From: " . strip_tags($from) . "\r\n";
             $headers .= "Reply-To: ". strip_tags($from) . "\r\n";
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 			
-			echo $to;
+			//echo $to;
 			echo $body;
 			echo $headers;
 			
 			
  
             mail($to,$subject,$body,$headers);
-			echo "send mail successfully";
+			//echo "send mail successfully";
+            header('location:confirm_mail.html');
         }
         else
         {
